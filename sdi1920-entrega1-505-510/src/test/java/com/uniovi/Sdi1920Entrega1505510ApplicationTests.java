0ddc155f330e62b9dacac9f2152d35ef465ad934
UO258935 <UO258935@uniovi.es>
2020-02-27 17:01:25 +0100

    Creación proyecto MYSOCIALNETWORK

---
+++ sdi1920-entrega1-505-510/src/test/java/com/uniovi/Sdi1920Entrega1505510ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi1920Entrega1505510ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
