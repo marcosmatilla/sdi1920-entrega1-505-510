9e5b78d802dd0507761a213eb07923467670e3bd
UO258935 <UO258935@uniovi.es>
2020-03-13 00:18:01 +0100

    Añadir post done

--- sdi1920-entrega1-505-510/src/main/java/com/uniovi/controllers/PostController.java
+++ sdi1920-entrega1-505-510/src/main/java/com/uniovi/controllers/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Post;
 import com.uniovi.entities.User;
@@ -27,5 +28,18 @@ public class PostController {
 		model.addAttribute("postList", posts.getContent());
 		model.addAttribute("page", posts);
 		return "post/list";
+	}
+	
+	@RequestMapping(value = "/post/add", method = RequestMethod.GET)
+	public String addPost(Model model) {
+		model.addAttribute("post", new Post());
+		return "post/add";
+	}
+
+	
+	@RequestMapping(value = "/post/add", method = RequestMethod.POST)
+	public String addPost(Post post, Model model) {
+		postsService.addPost(post, usersService.getCurrentUser());
+		return "redirect:/post/list";
 	}
 }
